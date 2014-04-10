49179a896d7125f42a86da9c592aa63e68ffdd7f
Joao Nunes <joao@Joaos-MacBook-Pro.local>
2014-04-10 14:35:19 +0300

    Fixes transparent transitions

--- VPImageCropperDemo/VPImageCropper/VPImageCropperViewController.m
+++ VPImageCropperDemo/VPImageCropper/VPImageCropperViewController.m
@@ -60,6 +60,9 @@
 }
 
 - (void)initView {
+    
+    self.view.backgroundColor = [UIColor blackColor];
+    
     self.showImgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
     [self.showImgView setMultipleTouchEnabled:YES];
     [self.showImgView setUserInteractionEnabled:YES];
